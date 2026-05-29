ca1c82118178574e91cafef83c2e7d0e3d1215e3
Akash Kumar <akashvidhuri6@gmail.com>
2026-05-29 21:42:19 +0530

    Sucess connection established with mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
+
 require("dotenv").config();
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -14,5 +19,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
