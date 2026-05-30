77af6a52477ebccddb131488ffe3dac5946676e3
Akash Kumar <akashvidhuri6@gmail.com>
2026-05-30 19:50:05 +0530

    CORS fixed

--- Backend/server.js
+++ Backend/server.js
@@ -2,37 +2,30 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const roadmapRoutes = require("./routes/roadmapRotes");
+
 require("dotenv").config();
 
 const app = express();
 
-// Connect MongoDB
 connectDB();
 
-// CORS Configuration
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
       "https://career-roadmap-generator-six.vercel.app",
+      "http://localhost:3000",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
-// Middleware
 app.use(express.json());
 
-// Routes
 app.use("/api/roadmap", roadmapRoutes);
 
-// Health Check Route
 app.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Career Roadmap API Running...",
-  });
+  res.send("Career Roadmap API Running...");
 });
 
 const PORT = process.env.PORT || 5000;
