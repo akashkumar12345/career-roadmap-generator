8772e1bd42df9068308a7a37a6fc8f40a38a0085
Akash Kumar <akashvidhuri6@gmail.com>
2026-05-30 19:16:35 +0530

    more changes related to cores

--- Backend/server.js
+++ Backend/server.js
@@ -2,37 +2,22 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const roadmapRoutes = require("./routes/roadmapRotes");
+
 require("dotenv").config();
 
 const app = express();
 
-// Connect MongoDB
 connectDB();
 
-// CORS Configuration
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://career-roadmap-generator-six.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
+app.options("*", cors());
 
-// Middleware
 app.use(express.json());
 
-// Routes
 app.use("/api/roadmap", roadmapRoutes);
 
-// Health Check Route
 app.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Career Roadmap API Running...",
-  });
+  res.send("Career Roadmap API Running...");
 });
 
 const PORT = process.env.PORT || 5000;
