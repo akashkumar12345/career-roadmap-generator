b6361a33c539cad71e27741d473f1177ba550770
Akash Kumar <akashvidhuri6@gmail.com>
2026-05-30 19:23:53 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -2,22 +2,37 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const roadmapRoutes = require("./routes/roadmapRotes");
-
 require("dotenv").config();
 
 const app = express();
 
+// Connect MongoDB
 connectDB();
 
-app.use(cors());
-app.options("*", cors());
+// CORS Configuration
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://career-roadmap-generator-six.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
+// Middleware
 app.use(express.json());
 
+// Routes
 app.use("/api/roadmap", roadmapRoutes);
 
+// Health Check Route
 app.get("/", (req, res) => {
-  res.send("Career Roadmap API Running...");
+  res.status(200).json({
+    success: true,
+    message: "Career Roadmap API Running...",
+  });
 });
 
 const PORT = process.env.PORT || 5000;
